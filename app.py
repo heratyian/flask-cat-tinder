d9c64fd42b983a5a7933849fe41888abdef0a84b
Ian Heraty <heratyian@gmail.com>
2016-07-24 20:12:31 -0500

    Add DB, add top_cat page

--- app.py
+++ app.py
@@ -1,9 +1,20 @@
-from flask import Flask, render_template, request
+'''
+IMPORTS
+'''
+from flask import Flask, render_template, request, g
 import requests
 from random import randint
+import sqlite3
 
-
+# flask app
 app = Flask(__name__)
+
+'''
+CONSTANTS
+'''
+
+# DATABASE
+DATABASE = 'cat_database.db'
 
 # API KEYS
 BASE_URL = 'https://api.flickr.com/services/rest/'
@@ -31,10 +42,12 @@ DATA = 'photos'
 PHOTO_LIST = 'photo'
 URL = 'url_m'
 
+'''
+ROUTES
+'''
 
 @app.route('/')
 def index():
-
     r = requests.get(BASE_URL, params=payload)
 
     json_response = r.json()
@@ -57,13 +70,69 @@ def index():
 
 @app.route('/', methods=['POST'])
 def more_cats():
-    print(request)
     image_id = request.form['id']
-
+    image_secret = request.form['secret']
+    image_url = request.form['url_m']
+    my_values = (image_id, image_secret, image_url)
+    my_fields = ('image_id', 'image_secret', 'image_url')
+    insert('cat_picks', my_fields, my_values)
     return index()
 
+@app.route('/top_cat/')
+def top_cat():
+    top_cat_url = ""
+    for pick in query_db('select image_url from cat_picks group by image_url order by count(image_url) desc'):
+        # my_list.append("{} has the url {}".format(pick['image_id'],
+        #                                           pick['image_url']))
+        top_cat_url = pick
+        break
+    # return render_template('top_cat.html', my_list=my_list)
+    return render_template('top_cat.html', top_cat_url=top_cat_url)
 
+'''
+DB methods
+'''
 
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    return db
+
+def init_db():
+    with app.app_context():
+        db = get_db()
+        with app.open_resource('schema.sql', mode='r') as f:
+            db.cursor().executescript(f.read())
+        db.commit()
+
+def insert(table, fields=(), values=()):
+    # g.db is the database connection
+    # cur = g.db.cursor()
+    cur = get_db().cursor()
+    query = 'INSERT INTO {} ({}) VALUES ({})'.format(
+        table,
+        ', '.join(fields),
+        ', '.join(['?'] * len(values))
+    )
+    cur.execute(query, values)
+    # g.db.commit()
+    get_db().commit()
+    id = cur.lastrowid
+    cur.close()
+    return id
+
+def query_db(query, args=(), one=False):
+    cur = get_db().execute(query, args)
+    rv = cur.fetchall()
+    cur.close()
+    return (rv[0] if rv else None) if one else rv
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ schema.sql
@@ -0,0 +1,6 @@
+drop table if exists cat_picks;
+create table cat_picks (
+  image_id TEXT,
+  image_secret TEXT,
+  image_url TEXT
+);
